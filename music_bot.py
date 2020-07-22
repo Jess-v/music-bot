fe71496470afe5d03e029574a60951fb74f67308
jess-v <jess@plantfriends.xyz>
2020-07-21 20:40:07 -0500

    removed old unnecessary function from music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -10,28 +10,6 @@ import asyncio
 
 bot = commands.Bot(command_prefix='.')
 
-
-async def client_in_channel(ctx):
-    voice = get(ctx.bot.voice_clients, guild=ctx.guild)
-    try:
-        if not voice.is_connected():
-            await ctx.send("im not in voice dummy")
-            return False
-    except:
-        await ctx.send("im not in voice dummy")
-        return False
-    try:
-        user_channel = ctx.message.author.voice.channel
-    except:
-        await ctx.send("ur not even in voice shut up")
-        return False
-    bot_channel = voice.channel
-    if user_channel != bot_channel:
-        await ctx.send("im not even in ur channel??")
-        return False
-    return True
-    
-
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
